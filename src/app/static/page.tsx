ad7f6e978d969da59b029d4cab52961968ffdca7
Thiago <thiagovt.dev@gmail.com>
2023-10-26 13:47:41 -0300

    add static page

---
+++ src/app/static/page.tsx
@@ -0,0 +1,15 @@
+
+// import ClientSideRender from "@/components/ClientSideRender";
+// import StaticRender from "@/components/StaticRender";
+
+// export default function Dynamic() {
+//   return (
+//     <main className="container">
+//       <h1 className="my-5">Como funcionam as renderizações do Next.js</h1>
+
+//       <ClientSideRender>
+//         <StaticRender/>
+//       </ClientSideRender>
+//     </main>
+//   );
+// }
