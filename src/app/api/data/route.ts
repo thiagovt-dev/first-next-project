143dd440b2d7441ef72b76e5c1535f947d7a51cc
Thiago Vasconcelos Teixeira <110790007+thiagovt-dev@users.noreply.github.com>
2023-10-26 10:28:18 -0300

    Update route.ts

--- src/app/api/data/route.ts
+++ src/app/api/data/route.ts
@@ -1,12 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextResponse } from "next/server";
 
-type Data = {
-  name: string;
-  timestamp: Date;
-};
 
-export async function GET(request: Data){
+export async function GET(){
     const timestamp = new Date
     return NextResponse.json({name: 'John Dea', timestamp})
 
