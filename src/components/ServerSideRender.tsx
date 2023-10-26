e273bf000f11ad80b4d42d136bcc82b001e8115b
Thiago <thiagovt.dev@gmail.com>
2023-10-26 12:57:58 -0300

    commit

--- src/components/ServerSideRender.tsx
+++ src/components/ServerSideRender.tsx
@@ -1,26 +1,26 @@
-// async function fetchData() {
-//   const data = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/data`, {
-//     cache: "no-store",
-//   });
-//   const json = await data.json();
-//   return json;
-// }
+async function fetchData() {
+  const data = await fetch(`${process.env.NEXT_PUBLIC_APIURL}/api/data`, {
+    cache: "no-store",
+  });
+  const json = await data.json();
+  return json;
+}
 
-// interface ApiResponse {
-//   name: string;
-//   timestamp: Date;
-// }
+interface ApiResponse {
+  name: string;
+  timestamp: Date;
+}
 
-// export default async function ServerSideRender() {
-//   const time: ApiResponse = await fetchData();
+export default async function ServerSideRender() {
+  const time: ApiResponse = await fetchData();
 
-//   return (
-//     <div>
-//       <>
-//         <h3>Gerado no servidor: </h3>
-//         <h2>{time.timestamp.toString()}</h2>
-//       </>
-//     </div>
-//   );
-// }
+  return (
+    <div>
+      <>
+        <h3>Gerado no servidor: </h3>
+        <h2>{time.timestamp.toString()}</h2>
+      </>
+    </div>
+  );
+}
 
