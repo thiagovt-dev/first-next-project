5f35781a44906cdcc7875ef52b2fc1eaab932c44
Thiago Vasconcelos Teixeira <110790007+thiagovt-dev@users.noreply.github.com>
2023-11-01 12:24:05 +0000

    update producut list

--- src/components/ProductsList.tsx
+++ src/components/ProductsList.tsx
@@ -1,39 +1,22 @@
 import { ProductType, fetchProducts } from "@/services/products";
-import { Col, Row } from "reactstrap";
 import ProductCard from "./ProductCard";
 
-async function loadPosts() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  return res.json();
-}
 
 const ProductsList = async () => {
-      const posts = await loadPosts();
-      console.log(posts);
-      return (
-        <div className="post-list">
-          {posts.map((post) => (
-            <div key={post.id} className="post-listing">
-              <h3 className="post-title">{post.title}</h3>
-              <p className="post-body">{post.body}</p>
-            </div>
-          ))}
-        </div>
-      );
-//     const products: ProductType[] = await fetchProducts();
-//     console.log(products);
+     const products: ProductType[] = await fetchProducts();
+     console.log(products);
 
-//   return (
-//     <>
-//       <div className=" row g-5">
-//         {products.map((product) => (
-//           <div key={product.id} className="col-md-6 col-lg-4 col-xl-3">
-//             <ProductCard product={product} />
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
+   return (
+     <>
+       <div className=" row g-5">
+         {products.map((product) => (
+           <div key={product.id} className="col-md-6 col-lg-4 col-xl-3">
+             <ProductCard product={product} />
+           </div>
+         ))}
+       </div>
+     </>
+   );
 };
 
 export default ProductsList;
