a072c40e4e80a2dd90e609934cd0fa7273b0f2f2
Thiago <thiagovt.dev@gmail.com>
2023-11-05 12:22:38 -0300

    remove product[id]

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,42 +1,42 @@
-import ProductDetails from "@/components/ProductComponents/ProductDetails";
-import { fetchProduct, fetchProducts } from "@/services/products";
-import { Metadata } from "next";
-import { redirect } from "next/navigation";
+// import ProductDetails from "@/components/ProductComponents/ProductDetails";
+// import { fetchProduct, fetchProducts } from "@/services/products";
+// import { Metadata } from "next";
+// import { redirect } from "next/navigation";
 
-type ParamsProps = {
-  params: { id: string };
-};
+// type ParamsProps = {
+//   params: { id: string };
+// };
 
-export const generateMetadata = async ({
-  params,
-}: ParamsProps): Promise<Metadata> => {
-  const product = await fetchProduct(params.id);
+// export const generateMetadata = async ({
+//   params,
+// }: ParamsProps): Promise<Metadata> => {
+//   const product = await fetchProduct(params.id);
 
-  return {
-    title: `${product.name}`,
-    description: `${product.description}`,
-    icons: {
-      icon: "/favicon.ico",
-    },
-  };
-};
-export const dynamicParams = false;
+//   return {
+//     title: `${product.name}`,
+//     description: `${product.description}`,
+//     icons: {
+//       icon: "/favicon.ico",
+//     },
+//   };
+// };
+// export const dynamicParams = false;
 
-export async function generateStaticParams() {
-  const products = await fetchProducts();
+// export async function generateStaticParams() {
+//   const products = await fetchProducts();
 
-  return products.map((product) => ({ id: product.id.toString() }));
-}
+//   return products.map((product) => ({ id: product.id.toString() }));
+// }
 
-const Product = async ({ params: { id } }: ParamsProps) => {
-  const product = await fetchProduct(id);
+// const Product = async ({ params: { id } }: ParamsProps) => {
+//   const product = await fetchProduct(id);
 
-  if (!product) redirect("/products");
+//   if (!product) redirect("/products");
 
-  return (
-    <div className="container mt-5">
-      <ProductDetails product={product} />
-    </div>
-  );
-};
-export default Product;
+//   return (
+//     <div className="container mt-5">
+//       <ProductDetails product={product} />
+//     </div>
+//   );
+// };
+// export default Product;
