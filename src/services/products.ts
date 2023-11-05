1967636574e6be8fd2e4a51fdddd90ea8a7073e1
Thiago <thiagovt.dev@gmail.com>
2023-11-05 10:21:33 -0300

    test to deploy

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,27 +1,27 @@
-export type ProductType = {
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  imageUrl: string;
-  inStock: number;
-};
+// export type ProductType = {
+//   id: number;
+//   name: string;
+//   description: string;
+//   price: number;
+//   imageUrl: string;
+//   inStock: number;
+// };
 
-export async function fetchProducts() {
-  const products: ProductType[] = await fetch(
-    `http://localhost:3000/api/products`
-  ).then((res) => res.json());
-  return products;
-}
-export async function fetchProduct(id: string | number) {
-  const product: ProductType = await fetch(
-    `http://localhost:3000/api/products/${id}`,
-    {
-      next: { revalidate: 10 },
-    }
-  ).then((res) => res.json());
-  return product;
-}
+// export async function fetchProducts() {
+//   const products: ProductType[] = await fetch(
+//     `http://localhost:3000/api/products`
+//   ).then((res) => res.json());
+//   return products;
+// }
+// export async function fetchProduct(id: string | number) {
+//   const product: ProductType = await fetch(
+//     `http://localhost:3000/api/products/${id}`,
+//     {
+//       next: { revalidate: 10 },
+//     }
+//   ).then((res) => res.json());
+//   return product;
+// }
 
 // http://localhost:3000/
 //process.env.NEXT_PUBLIC_APIURL
